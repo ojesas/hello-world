b9e63463855642c11fa01abbc4e6ba024d49fc3b
O Jesas <jesaiassa@gmail.com>
2023-07-21 11:00:03 -0300

    Incrementando outputs

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,26 @@
-console.log("Hello World");
+//console.log("Hello World");
+
+
+//Criando letras grandes com composição de caracteres
+let hH, eE, lL, oO;
+
+//Cria/draw letra H
+hH = `#     #\n
+#     #\n
+#  #  #\n
+#     #\n
+#     #\n`;
+
+console.log(hH);
+console.log();
+//Cria/draw letra H
+eE = `#  #  #\n
+#      \n
+#  #  #\n
+#      \n
+#  #  #\n`;
+
+console.log(eE);
+
 
 
